b0758fd55e114c9856af7f08e11ffdf10df28f97
kreativejosh <kreativejosh@gmail.com>
2018-04-13 16:45:56 +0100

    code for figma design

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Implementation of my figma design</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+	<div class="firstsection">
+		<button class="button">JOIN NOW!</button>
+		<h1 class="headnote">JOIN <strong style="color:#e1cc06; ">HNG INTERNSHIP</strong><br> TO BECOME<br> <strong style="color: #e1cc06">WORLD CLASS.</strong></h1>	
+
+	</div>
+
+	<div class="secondsection">
+		<h1 class="timess"><?php
+    $currentDateTime = date('Y-m-d H:i:s');
+    echo $currentDateTime;
+?></h1>
+		<h3>Limitless time...</h3>
+	</div>
+
+	<div class="thirdsection">
+		<h3 class="spn">Sponsors</h3>
+		<blockquote><p>Grateful to hotelsNG and following partners for bringing to reality, this internship. Learn without cause!</p></blockquote>
+		<div style="padding-left: 450px;margin-top: 100px;">
+			<span><img src="img/bct.png"></span>
+			<span><img src="img/ora.png"></span>
+			<span><img src="img/fig.png"></span>
+			<span><img src="img/hng.png"></span>
+		</div>
+		<p>Designed by Kreativejosh</p>
+	</div>
+</body>
+</html>
